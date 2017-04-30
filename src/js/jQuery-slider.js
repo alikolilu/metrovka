188720f3623d30a9c86a37ab0528a9a7e84e34e3
Alina Ivashchenko <7aliko212@gmail.com>
2017-04-30 19:30:13 +0300

    debbuging

--- src/js/jQuery-slider.js
+++ src/js/jQuery-slider.js
@@ -1,49 +1,49 @@
-(function( $ ) {
-  $.fn.mySlider = function() {
+(function( $ ){
 
-    var metrovkaSlider = {
-      infinet: true,
-      index: 0,
-      slidesVisible: 1,
-      slidesToScroll: 1;
-      arrows: true,
-      swipeAble: true,
-      pagination: true,
-      autoplay: true,
-      autoplaySpeed: 1000
-  };
+$.fn.slider = function(){
+      var defaults = {};
+      defaults.infinet = true;
+      defaults.index = 0;
+      defaults.slidesVisible = 1;
+      defaults.slidesToScroll = 1;
+      defaults.arrows = true;
+      defaults.swipeAble = true;
+      defaults.pagination = true;
+      defaults.autoplay = true;
+      defaults.autoplaySpeed = 1000;
   
-    var settings = $.extend({}, metrovkaSlider, settings);
-    var part = this;
-    var partWidth = 202;
+    var options = $.extend({}, defaults, options);
+    var elem = this;
+    var elemWidth = 202;
     var slides = $('.slide');
     var numberofElem = length.slides;
+    //$('#slidesContainer').css('overflow', 'hidden');
       $('#slidesContainer').css('left','0px');
       $('#slidesContainer').css('position','relative');
-      $('#slidesContainer').css('width','partWidth*numberofElem');
-   
+      $('#slidesContainer').css('width','elemWidth*numberofElem');
+   // $('#slidesContainer>div:last-child').prependTo('#slidesContainer');
 
-    slidesVisible = part.width/partWidth>>0;
+    slidesVisible = elem.width/elemWidth>>0;
       $('<span class ="control_next">Next</span>').appendTo('#slidesShow');
       $('<span class = "control_prev">Prev</span>').prependTo('#slidesShow');
      
  $('.control_next').click(function(){
               $('#slidesContainer').animate({
-                left: -partWidth
+                left: -elemWidth
                }, 1000, function(){
                  $('#slidesContainer>div:first-child').appendTo('#slidesContainer');
-                 settings.index != 0
-               ? settings.index-1 : settings.index = numberofElem-1;
+                 options.index != 0
+               ? options.index-1 : options.index = numberofElem-1;
                 $('#slidesContainer').css('left','0px')
                 });
              }); 
     $('.control_prev').click(function(){
                 $('#slidesContainer').animate({
-                left: +partWidth    
+                left: +elemWidth    
                }, 1000, function(){
               $('#slidesContainer>div:last-child').prependTo('#slidesContainer');
-                 settings.index != numberofElem-1
-               ? settings.index+1 : settings.index = 0;
+                 options.index != numberofElem-1
+               ? options.index+1 : options.index = 0;
                   $('#slidesContainer').css('left','0px')
 
                });
@@ -55,22 +55,22 @@ $( window ).on( "swipe", function( event ) {
   $('slidesContainer').animate({
     left: +swipestart-swipeend
   }, 1000, function(){
-    for(var i = 1; i<=math.abs((swipestart-swipeend)/partWidth >>0); i++){
+    for(var i = 1; i<=math.abs((swipestart-swipeend)/elemWidth >>0); i++){
       if ((swipestart-swipeend)<0){
         $('#slidesContainer').animate({
-                left: -partWidth
+                left: -elemWidth
                }, 1000, function(){
-                 settings.index != 0
-               ? settings.index-1 : settings.index = numberofElem-1;
+                 options.index != 0
+               ? options.index-1 : options.index = numberofElem-1;
                 $('#slidesContainer>div:first-child').appendTo('#slidesContainer');
                 $('#slidesContainer').css('left','0px')
                    })
       }else{
         $('#slidesContainer').animate({
-                left: +partWidth
+                left: +elemWidth
                }, 1000, function(){
-                 settings.index != numberofElem-1
-               ? settings.index+1 : settings.index = 0;
+                 options.index != numberofElem-1
+               ? options.index+1 : options.index = 0;
                 $('#slidesContainer>div:last-child').prependTo('#slidesContainer');
                 $('#slidesContainer').css('left','0px')
                });
@@ -84,16 +84,19 @@ $( window ).on( "swipe", function( event ) {
 $('#slidesContainer').mouseout(function(){
   setInterval(function () {
     $('#slidesContainer').animate({
-                left: -partWidth
+                left: -elemWidth
                }, 1000, function(){
-                 settings.index != 1
-               ? settings.index-1 : optionsindex = numberofElem;
+                 options.index != 1
+               ? options.index-1 : optionsindex = numberofElem;
                  $('#slidesContainer>div:first-child').appendTo('#slidesContainer');
                  $('#slidesContainer').css('left','0px')
                })
-  }, settingss.autoplaySpeed);
+  }, options.autoplaySpeed);
 });
-  };
 
-})(jQuery);
+};
+}(jQuery))
 
+$(document).ready(function(){
+  $('.slidesShow').slider();
+})
